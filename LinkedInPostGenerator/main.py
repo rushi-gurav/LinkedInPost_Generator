c976fe300e0e46121e4b1a58a10f23caefffffeb
rushi-gurav <142071046+rushi-gurav@users.noreply.github.com>
2025-06-18 22:44:58 +0530

    Update main.py

--- LinkedInPostGenerator/main.py
+++ LinkedInPostGenerator/main.py
@@ -10,7 +10,7 @@ language_options = ["English", "Hinglish"]
 
 # Main app layout
 def main():
-    st.subheader("LinkedIn Post Generator: ")
+    st.subheader("LinkedIn Post Generator: By Rushikesh Gurav")
 
     # Create three columns for the dropdowns
     col1, col2, col3 = st.columns(3)
